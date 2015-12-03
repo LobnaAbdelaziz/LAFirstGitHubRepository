36e557c0673d2ac9d30ec44b9ac289d1121fe281
lobna_abdelaziz <lobna_abdelaziz@hotmail.com>
2015-12-03 15:17:51 -0700

    initial

---
+++ VolunteerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VolunteerApp.MvcApplication" Language="C#" %>
---
+++ VolunteerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VolunteerApp.Startup))]
+namespace VolunteerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app); // mjbtest
+        }
+    }
+}
